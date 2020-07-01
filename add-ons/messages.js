b39d488d63d4036dd3046408f6f12362dd2776e6
Isaac Owusu <32453300+Ice745@users.noreply.github.com>
2020-07-01 10:54:01 +0000

    Tokens removed

--- add-ons/messages.js
+++ add-ons/messages.js
@@ -1,6 +1,6 @@
 const twilio = require('twilio')
-const accSid = 'AC11c7632acaf27e1572fec285c9a1c4d4'
-const auth = '5d2dd50e844d8fc9af2c990e09cac395'
+const accSid = ''
+const auth = ''
 
 const client = twilio(accSid,auth);
 let send = function (sender,reciepient,amount,balance){
@@ -8,7 +8,7 @@ let send = function (sender,reciepient,amount,balance){
     client.messages.create({
         body:`You have transferred GHS ${amount} to ${reciepient} \n\nYour new balance is ${balance}`,
         to: `${sender}`,
-        from: '+14842323311'
+        from: ''
     })
     .then((message)=> {
         console.log(message.sid)
